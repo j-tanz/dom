cd2187584a742705401004c3bf14868f70aad2c9
tanz <john.tansor@gmail.com>
2018-01-23 11:39:25 -0600

    adds dom.js

---
+++ dom.js
@@ -0,0 +1,60 @@
+var $ = function(sel){
+    var elem = document.querySelectorAll(sel);
+
+    var text = function(str) {
+        for (var i = 0; i < elem.length; i++) {
+            elem[i].innerText = str;
+        }
+    }
+    
+    var addClass = function(className) {
+        for (var i = 0; i < elem.length; i++) {
+            elem[i].classList.add(className);
+         } 
+    }
+
+    var removeClass = function(className) {
+        for (var i = 0; i < elem.length; i++) {
+            elem[i].classList.remove(className);
+        }
+    }
+
+    var on = function(action, cb)  {
+        for (var i = 0; i < elem.length; i++) {
+            elem[i].addEventListener(action, cb);
+        }
+    }
+
+    var html = function(newHTML) {
+        for (var i = 0; i < elem.length; i++) {
+            elem[i].innerHTML = newHTML;
+        }
+    }
+
+    var toggleClass = function(className) {
+        for (var i = 0; i < elem.length; i++){
+
+            if (elem[i].className.includes(className)) {
+            elem[i].classList.remove(className);
+          } else {
+            elem[i].classList.add(className); 
+          }
+        }
+    }
+        console.log(className);
+    }
+
+
+    //toggleClass
+    
+
+    var publicAPI = {
+        element: elem,
+        text: text,
+        addClass: addClass,
+        removeClass: removeClass,
+        on: on,
+        html: html
+    }
+        return publicAPI;
+    
